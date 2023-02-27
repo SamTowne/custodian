d0ce1981775db51439f9391ad739d435533a62ec
Sam Towne <sam.towne2@gmail.com>
2023-02-27 16:19:56 -0700

    log group variables

--- infra/iam.tf
+++ infra/iam.tf
@@ -2,5 +2,5 @@ module "lambda-iam" {
   source                   = "./modules/lambda-iam"
   processor_log_group_name = "aws/lambda/cloud_custodian_processor"
   scheduler_log_group_name = "aws/lambda/cloud_custodian_scheduler"
-  dynamodb_updater_log_group_name = "aws/lambda/cloud_custodian_dynamodb_updater"
+  dynamodb_updater_log_group_name = "aws/lambda/cloud_custodian_configurator"
 }
--- infra/modules/lambda-iam/variables.tf
+++ infra/modules/lambda-iam/variables.tf
@@ -1,1 +1,3 @@
 variable "processor_log_group_name" {}
+variable "scheduler_log_group_name" {}
+variable "configurator_log_group_name" {}
