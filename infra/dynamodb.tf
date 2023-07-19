78e249c0b800510e17364c6dc60924d4a7713292
Sam Towne <sam.towne2@gmail.com>
2023-07-19 07:10:21 -0700

    destroy non-bootstrap infra (#27)

--- infra/dynamodb.tf
+++ infra/dynamodb.tf
@@ -1,25 +1,25 @@
-resource "aws_dynamodb_table" "cloud_custodian_policy_config" {
-    name = "cloud_custodian_policy_config"
-    hash_key = "rule_id"
-    billing_mode = "PAY_PER_REQUEST"
+# resource "aws_dynamodb_table" "cloud_custodian_policy_config" {
+#     name = "cloud_custodian_policy_config"
+#     hash_key = "rule_id"
+#     billing_mode = "PAY_PER_REQUEST"
 
-    attribute {
-      name = "rule_id"
-      type = "N"
-    }
+#     attribute {
+#       name = "rule_id"
+#       type = "N"
+#     }
     
-    attribute {
-      name = "is_active"
-      type = "S"
-    }
+#     attribute {
+#       name = "is_active"
+#       type = "S"
+#     }
 
-    global_secondary_index {
-      name = "is_active_index"
-      hash_key = "is_active"
-      projection_type = "ALL"
-    }
+#     global_secondary_index {
+#       name = "is_active_index"
+#       hash_key = "is_active"
+#       projection_type = "ALL"
+#     }
 
-    point_in_time_recovery {
-      enabled = true
-    }
-}
+#     point_in_time_recovery {
+#       enabled = true
+#     }
+# }
